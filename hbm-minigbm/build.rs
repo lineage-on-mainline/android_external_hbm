9de387452cad5d12bdab7c8e64e2767ac09c03c2
Chia-I Wu <olvaffe@gmail.com>
2024-08-14 10:06:36 -0700

    hbm-minigbm: add a symlink for the generated header

--- hbm-minigbm/build.rs
+++ hbm-minigbm/build.rs
@@ -1,30 +1,46 @@
 // Copyright 2024 Google LLC
 // SPDX-License-Identifier: MIT
 
-use std::env;
 use std::path::PathBuf;
+use std::{env, fs, os::unix};
 
 fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let pkg_name = env::var("CARGO_PKG_NAME").unwrap().replace('-', "_");
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    let copyright = "// Copyright 2024 Google LLC\n// SPDX-License-Identifier: MIT";
     let hdr_name = format!("{}.h", pkg_name);
-    let hdr_guard = format!("{}_H", pkg_name.to_uppercase());
-    let out_path = PathBuf::from(out_dir).join(hdr_name);
+    let out_path = PathBuf::from(out_dir).join(&hdr_name);
 
-    cbindgen::Builder::new()
-        .with_crate(manifest_dir)
-        .with_header(copyright)
-        .with_sys_include("sys/types.h")
-        .with_include_guard(hdr_guard)
-        .with_include_version(true)
-        .with_language(cbindgen::Language::C)
-        .with_cpp_compat(true)
-        .generate()
-        .expect("failed to generate bindings")
-        .write_to_file(out_path);
+    let _changed = match cbindgen::generate(&manifest_dir) {
+        Ok(bindings) => bindings.write_to_file(&out_path),
+        Err(cbindgen::Error::ParseSyntaxError { .. }) => false,
+        Err(err) => panic!("{:?}", err),
+    };
+
+    // this is not guaranteed, but out_path should be
+    // <workspace>/target/<profile>/build/<pkg_name-hash>/out/<hdr_name>
+    let depth = out_path.components().count();
+    if depth > 6 {
+        let mut link_path = PathBuf::new();
+        let mut link_target = PathBuf::new();
+        for (idx, comp) in out_path.components().enumerate() {
+            if idx + 4 < depth {
+                link_path.push(comp);
+            } else {
+                link_target.push(comp);
+            }
+        }
+        link_path.push(hdr_name);
+
+        if link_path.is_symlink() && link_path.read_link().unwrap() != link_target {
+            let _ = fs::remove_file(&link_path);
+        }
+
+        if !link_path.exists() {
+            unix::fs::symlink(link_target, link_path).expect("failed to create symlink");
+        }
+    }
 
     println!("cargo:rerun-if-changed=src");
 }
