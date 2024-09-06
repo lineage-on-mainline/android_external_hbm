8eeeb1b37169af93b63a32561c67b8c695d2c941
Ryan Neph <ryanneph@google.com>
2024-09-06 12:33:35 -0700

    hbm-minigbm: tweak bindgen match expression
    The resulting value of the match expression is unused, so tweak the
    match block such that it doesn't even produce a value.

--- hbm-minigbm/build.rs
+++ hbm-minigbm/build.rs
@@ -12,9 +12,11 @@ fn main() {
     let hdr_name = format!("{}.h", pkg_name);
     let out_path = PathBuf::from(out_dir).join(&hdr_name);
 
-    let _changed = match cbindgen::generate(manifest_dir) {
-        Ok(bindings) => bindings.write_to_file(&out_path),
-        Err(cbindgen::Error::ParseSyntaxError { .. }) => false,
+    match cbindgen::generate(manifest_dir) {
+        Ok(bindings) => {
+            bindings.write_to_file(&out_path);
+        }
+        Err(cbindgen::Error::ParseSyntaxError { .. }) => {}
         Err(err) => panic!("{:?}", err),
     };
 
