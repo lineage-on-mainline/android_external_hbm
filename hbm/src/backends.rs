lineage-on-mainline/android_external_hbm

commit 393ae00dd80a1b22a23cd5eab8944bb8707436df

hbm: check flags in Description::is_valid

hbm/src/backends.rs
@@ -55,6 +55,12 @@ impl Description {
     }
 
     pub(crate) fn is_valid(&self) -> bool {
+        // the bo is useless if none of these flags is set
+        let min_flags = Flags::EXTERNAL | Flags::MAP | Flags::COPY;
+        if !self.flags.intersects(min_flags) {
+            return false;
+        }
+
         if self.is_buffer() {
             self.modifier.is_invalid()
         } else {

hbm/src/bo.rs
@@ -154,6 +154,7 @@ impl Bo {
             return Error::user();
         }
 
+        // TODO if the bo cannot be mapped nor copied, all we need is the dma-buf
         let backend = self.device.backend(self.backend_index);
         backend.bind_memory(&mut self.handle, mt, dmabuf)?;
 

commit 15de44eb51479556413db9f7a90402ad503cd3eb

hbm: fix test_description
Fixes: 393ae00 ("hbm: check flags in Description::is_valid")
Closes: https://gitlab.freedesktop.org/olv/hbm/-/issues/1

hbm/src/backends.rs
@@ -687,10 +687,17 @@ mod tests {
 
     #[test]
     fn test_description() {
+        // at least one of EXTERNAL, MAP, or COPY should be set
         let mut desc = Description::new();
-        assert!(desc.is_valid());
+        assert!(!desc.is_valid());
+        for flag in [Flags::EXTERNAL, Flags::MAP, Flags::COPY] {
+            desc = desc.flags(flag);
+            assert!(desc.is_valid());
+        }
+
         assert!(desc.is_buffer());
 
+        // buffer cannot have a modifier
         desc = desc.modifier(formats::MOD_LINEAR);
         assert!(!desc.is_valid());
 